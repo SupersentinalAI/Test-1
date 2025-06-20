5bd404430f7577a3f58e2e923118fd84064fd219
SupersentinalAI <varunadev2000@gmail.com>
2025-06-20 22:13:01 +0530

    Update context.py

--- core/context.py
+++ core/context.py
@@ -7,16 +7,10 @@ class Context:
         openai_key=None,
         dhan_client_id=None,
         dhan_access_token=None,
-        ws_token=None,
-        telegram_token=None,
-        telegram_chat_id=None
     ):
         self.openai_key = openai_key or os.getenv("OPENAI_KEY")
         self.dhan_client_id = dhan_client_id or os.getenv("DHAN_CLIENT_ID")
         self.dhan_access_token = dhan_access_token or os.getenv("DHAN_ACCESS_TOKEN")
-        self.ws_token = ws_token or os.getenv("WS_TOKEN")
-        self.telegram_token = telegram_token or os.getenv("TELEGRAM_TOKEN")
-        self.telegram_chat_id = telegram_chat_id or os.getenv("TELEGRAM_CHAT_ID")
         self._dhan_http = None  # Lazy-loaded
 
     def get_dhan_http(self):
